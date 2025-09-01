1c7ea7c0d058449700e60f476ad62090a6a14fc9
juanklan11 <57101853+juanklan11@users.noreply.github.com>
2025-09-01 17:12:04 +1000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,24 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
 
 export const metadata: Metadata = {
   title: "Childcare Energy – NEPI",
