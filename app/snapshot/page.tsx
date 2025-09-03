a5c35522855e95cacbeff769aaae599d33a17876
juanklan11 <57101853+juanklan11@users.noreply.github.com>
2025-09-03 16:25:52 +1000

    Update page.tsx

--- app/snapshot/page.tsx
+++ app/snapshot/page.tsx
@@ -1,3 +1,9 @@
 import dynamic from "next/dynamic";
-const ParentSnapshot = dynamic(() => import("@/components/ParentSnapshot"), { ssr: false });
-export default function Page(){ return <ParentSnapshot/>; }
+
+const ParentSnapshot = dynamic(() => import("@/components/ParentSnapshot"), {
+  ssr: false,
+});
+
+export default function Page() {
+  return <ParentSnapshot />;
+}
